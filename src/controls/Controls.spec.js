f55b5222cc5cb9485d9b5e78f16fa7dc7dc7379e
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-08-19 16:18:24 -0700

    completed Controls and MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -13,28 +13,49 @@ describe("<Controls />", () => {
   });
 });
 
-describe("buttons", () => {
-  it("when unlocked/open shows green-led", () => {
-    const { queryByText } = render(<Controls locked={false} closed={false} />);
-    expect(queryByText(/open/i).className.includes("green-led")).toBeTruthy();
-    expect(
-      queryByText(/unlocked/i).className.includes("green-led")
-    ).toBeTruthy();
+describe("buttons exist", () => {
+  it("shows buttons to toggle", () => {
+    const { getByTestId } = render(<Controls locked={false} closed={false} />);
+    expect(getByTestId("open-close-btn")).toBeTruthy();
+    expect(getByTestId("lock-unlock-btn")).toBeTruthy();
   });
 });
 
 //when clicked, text changes
-describe("buttons", () => {
+describe("buttons text changes", () => {
   it("button text changes when clicked", () => {
-    const click = jest.fn();
+    const { getByTestId, queryByText } = render(
+      <Controls locked={false} closed={false} />
+    );
+    const openCloseBtn = getByTestId("open-close-btn");
+    const lockUnlockBtn = getByTestId("lock-unlock-btn");
 
-    const { getByText } = render(<Controls disabled={!closed} />);
+    fireEvent.click(openCloseBtn);
+    expect(queryByText(/open/i)).toBeTruthy;
 
-    const button = getByText(/speak/i);
-    fireEvent.click(button);
+    fireEvent.click(lockUnlockBtn);
+    expect(queryByText(/lock/i)).toBeTruthy;
 
-    expect(click).toHaveBeenCalled();
+    fireEvent.click(lockUnlockBtn);
+    expect(queryByText(/unlock/i)).toBeTruthy;
 
-    console.log(button);
+    fireEvent.click(openCloseBtn);
+    expect(queryByText(/close/i)).toBeTruthy;
+  });
+});
+
+describe("closed button disabled", () => {
+  it("closed button doesnt work if gate locked", () => {
+    const { getByTestId } = render(<Controls locked={true} closed={false} />);
+
+    expect(getByTestId("open-close-btn").disabled).toBeTruthy();
+  });
+});
+
+describe("locked button disabled", () => {
+  it("lock button doesnt work if gate open", () => {
+    const { getByTestId } = render(<Controls locked={false} closed={false} />);
+
+    expect(getByTestId("lock-unlock-btn").disabled).toBeTruthy();
   });
 });
